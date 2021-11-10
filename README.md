99348942c81c51967f2032fa15c630f8d899cfdc
pabloduran016 <pdrm56@gmail.com>
2021-11-10 20:12:11 +0100

    Add tasks to project. PUSH

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # PYDONE
 Todoist sync aplication with python. Application to update data from todist and upload datat to todist using a txt
 
+## Getting started:
+First, install all the libraries:  
+```bash
+pip install -r requirements.txt
+``` 
+Then, create an account.json file with a key being "token":  
+```json 
+{"token": "Your API token"}
+```
+By default, the app while look for a file named `account.json` in the current directory. You can 
+specify a custom file using the `-c` flag.  
+You can get your API token in the Todoist app in Settings -> Integrations.
+
+
 ## Usage:
 ### Commands:
-    `push <file_path>`: Update todoist account from `file`
-    `pull <file_path>`: update `file` from todoist account
+`push <file_path>` Update todoist account from `file`  
+`pull <file_path>` Update `file` from todoist account  
 
 ### Subcommands:
-    `-c <file_path>`: Choose todist account from cutstom .json `file`
+`-c <file_path>` Choose todist account from cutstom .json `file` with the format determined before  
+`-p <project_name>` Choose todist project to work from  
 
 ## References:
-- [Todist API Docs](https://developer.todoist.com/sync/v8/#get-all-projects)
+- [Todist API Docs](https://developer.todoist.com/sync/v8/#get-all-projects)  
+- [Todoist API Module Docs](https://todoist-python.readthedocs.io/en/latest/)  
+- [Todoist GitHub Repo](https://github.com/doist/todoist-python)  
--- main.py
+++ main.py
@@ -1,28 +1,57 @@
+import traceback
 from dataclasses import dataclass
 import sys
-from typing import Optional
+from typing import Optional, List
 from enum import auto, Enum
+import todoist
+import json
 
 FILE_PATH = 'TODO.txt'
 
 
 @dataclass
 class TodistAccount:
-    pass
+    token: str
 
 
-DEFAULT_ACCOUNT = TodistAccount()
+def _set_api(account: TodistAccount) -> todoist.TodoistAPI:
+    api = todoist.TodoistAPI(account.token)
+    api.sync()
+    return api
+
+
+def _add_task(api: todoist.TodoistAPI, project_id: int, task: str):
+    api.items.add(task, project_id=project_id)
+    api.commit()
+
+
+def _add_tasks(api: todoist.TodoistAPI, project_id: int, tasks: List[str]):
+    for t in tasks:
+        _add_task(api, project_id, t)
 
 
 def load_account_from_json(path: str) -> TodistAccount:
-    raise NotImplementedError
+    with open(path, 'r') as f:
+        data = json.load(f)
+        if 'token' not in data:
+            ValueError('Invalid json file. Expected key `token`')
+        return TodistAccount(data['token'])
 
 
-def update_todist_from_file(path: str, account: TodistAccount) -> bool:
-    raise NotImplementedError
+def update_todist_from_file(account: TodistAccount, path: str, project_name: str) -> bool:
+    try:
+        api = _set_api(account)
+        project_id = next(filter(lambda x: x['name'] == project_name, api.projects.all()))['id']
+        with open(path, 'r') as f:
+            tasks = [raw_line.replace('-', '').strip() for raw_line in f.readlines()]
+        _add_tasks(api, project_id, tasks)
+        return True
+    except Exception:
+        traceback.print_exc()
+        return False
 
 
-def update_file_from_todoist(path: str, account: TodistAccount) -> bool:
+def update_file_from_todoist(account: TodistAccount, path: str, project_name: str) -> bool:
     raise NotImplementedError
 
 
@@ -32,11 +61,12 @@ class OpType(Enum):
 
 USAGE = """USAGE:
   COMMAND:
-    push <file_path>: update todoist account from `file`
-    pull <file_path>: update `file` from todoist account
+    push <file_path>: Update todoist account from `file`
+    pull <file_path>: Update `file` from todoist account
   
   SUBCOMMAND:
-    -c <file_path>: Choose todist account from cutstom .json `file`"""
+    -c <file_path>: Choose todist account from cutstom .json `file`
+    -p <project_name>: Choose todist project to work from"""
 
 if __name__ == '__main__':
     args = list(reversed(sys.argv[1:].copy()))
@@ -44,13 +74,17 @@ if __name__ == '__main__':
     op: Optional[OpType] = None
     custom: bool = False
     path_to_custom: str = ''
+    project_name: str = 'Inbox'
     if len(args) == 0:
         print(USAGE)
         exit(1)
     while len(args) > 0:
         arg = args.pop()
         if arg == 'push':
-            if len(args) > 0:
+            if len(args) == 0:
+                op = OpType.PUSH
+                path = FILE_PATH
+            elif len(args) > 0:
                 op = OpType.PUSH
                 path = args.pop()
             else:
@@ -58,7 +92,10 @@ if __name__ == '__main__':
                 print(USAGE)
                 exit(1)
         elif arg == 'pull':
-            if len(args) > 0:
+            if len(args) == 0:
+                op = OpType.PULL
+                path = FILE_PATH
+            elif len(args) > 0:
                 op = OpType.PULL
                 path = args.pop()
             else:
@@ -73,6 +110,13 @@ if __name__ == '__main__':
                 print('Invalid usage for `-c` subcommand')
                 print(USAGE)
                 exit(1)
+        elif arg == '-p':
+            if len(args) > 0:
+                project_name = args.pop()
+            else:
+                print('Invalid usage for `-`p` subcommand')
+                print(USAGE)
+                exit(1)
         else:
             print(f'Unknwon command {arg}')
             exit(1)
@@ -80,17 +124,17 @@ if __name__ == '__main__':
         print(f'[INFO] Loading custom account from file: `{path_to_custom}`')
         account = load_account_from_json(path_to_custom)
     else:
-        account = DEFAULT_ACCOUNT
+        account = load_account_from_json('account.json')
     succeeded: bool = False
     if op == OpType.PUSH:
-        print(f'[INFO] Pushing file `{path}` to todoist')
-        succeeded = update_todist_from_file(path, account)
+        print(f'[INFO] Pushing file `{path}` to todoist. Project: {project_name}')
+        succeeded = update_todist_from_file(account, path, project_name)
     elif op == OpType.PULL:
-        print(f'[INFO] Pulling to file `{path}` from todoist')
-        succeeded = update_file_from_todoist(path, account)
+        print(f'[INFO] Pulling to file `{path}` from todoist. Project: {project_name}')
+        succeeded = update_file_from_todoist(account, path, project_name)
     else:
         raise ValueError(f'Unknown operation {op}. This may be a bug in the parsing of the arguments')
     if succeeded:
-        print('[SUCCESS] Succesfully finished exedcution')
+        print('[SUCCESS] Succesfully finished execution')
     else:
         print('[ERROR] Execution failed, Try again')
