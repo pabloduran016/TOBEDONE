851dd5a4d754a2032aae228ede86dd1556cf55d6
pabloduran016 <pdrm56@gmail.com>
2021-11-10 21:41:07 +0100

    TOBEDONE now checks for duplicates

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class TodistAccount:
     token: str
 
 
-def _set_api(account: TodistAccount) -> todoist.TodoistAPI:
-    api = todoist.TodoistAPI(account.token)
+def _set_api(acc: TodistAccount) -> todoist.TodoistAPI:
+    api = todoist.TodoistAPI(acc.token)
     api.sync()
     return api
 
@@ -27,25 +27,27 @@ def _add_task(api: todoist.TodoistAPI, project_id: int, task: str):
 
 
 def _add_tasks(api: todoist.TodoistAPI, project_id: int, tasks: List[str]):
+    current_tasks = [e['content'] for e in api.projects.get_data(project_id)['items']]
     for t in tasks:
-        _add_task(api, project_id, t)
+        if t not in current_tasks:
+            _add_task(api, project_id, t)
 
 
-def load_account_from_json(path: str) -> TodistAccount:
-    if not os.path.exists(path):
+def load_account_from_json(file_path: str) -> TodistAccount:
+    if not os.path.exists(file_path):
         print(f'[ERROR] Coulnn\'t find path `{path}`')
-    with open(path, 'r') as f:
+    with open(file_path, 'r') as f:
         data = json.load(f)
         if 'token' not in data:
             ValueError('Invalid json file. Expected key `token`')
         return TodistAccount(data['token'])
 
 
-def update_todist_from_file(account: TodistAccount, path: str, project_name: str) -> bool:
+def update_todist_from_file(acc: TodistAccount, file_path: str, project_name: str) -> bool:
     try:
-        api = _set_api(account)
+        api = _set_api(acc)
         project_id = next(filter(lambda x: x['name'] == project_name, api.projects.all()))['id']
-        with open(path, 'r') as f:
+        with open(file_path, 'r') as f:
             tasks = [raw_line.replace('-', '').strip() for raw_line in f.readlines()]
         _add_tasks(api, project_id, tasks)
         return True
@@ -54,13 +56,15 @@ def update_todist_from_file(account: TodistAccount, path: str, project_name: str
     return False
 
 
-def update_file_from_todoist(account: TodistAccount, path: str, project_name: str) -> bool:
+def update_file_from_todoist(acc: TodistAccount, file_path: str, project_name: str) -> bool:
     try:
-        api = _set_api(account)
+        api = _set_api(acc)
         project_id = next(filter(lambda x: x['name'] == project_name, api.projects.all()))['id']
         tasks = [e['content'] for e in api.projects.get_data(project_id)['items']]
-        with open(path, 'a') as f:
-            f.writelines([f'- {t}\n' for t in tasks])
+        with open(file_path, 'r') as f:
+            current_tasks = [raw_line.replace('-', '').strip() for raw_line in f.readlines()]
+        with open(file_path, 'a') as f:
+            f.writelines([f'- {t}\n' for t in tasks if t not in current_tasks])
         return True
     except Exception:
         traceback.print_exc()
@@ -75,12 +79,12 @@ class OpType(Enum):
 OP_NAMES = {'push': OpType.PUSH, 'pull': OpType.PULL}
 
 
-def load_config_from_json(path: str) -> Tuple[TodistAccount, str, OpType, str]:
-    with open(path, 'r') as f:
+def load_config_from_json(file_path: str) -> Tuple[TodistAccount, str, OpType, str]:
+    with open(file_path, 'r') as f:
         data = json.load(f)
         for k in ('account', 'project', 'action', 'file_path'):
             if k not in data:
-                print(f'[ERROR] Missing key `{k}` in config file `{path}`')
+                print(f'[ERROR] Missing key `{k}` in config file `{file_path}`')
                 exit(1)
         return load_account_from_json(data['account']), data['file_path'], OP_NAMES[data['action']], data['project']
 
@@ -89,28 +93,28 @@ def load_config_from_args(args: List[str]) -> Tuple[TodistAccount, str, OpType, 
     if len(args) == 0:
         print(USAGE)
         exit(1)
-    path: str = FILE_PATH
+    file_path: str = FILE_PATH
     account_path: str = 'accounts.tobedone.json'
     project_name: str = 'Inbox'
-    op: Optional[OpType] = None
+    action: Optional[OpType] = None
     while len(args) > 0:
         arg = args.pop()
         if arg == 'push':
             if len(args) == 0:
-                op = OpType.PUSH
+                action = OpType.PUSH
             elif len(args) > 0:
-                op = OpType.PUSH
-                path = args.pop()
+                action = OpType.PUSH
+                file_path = args.pop()
             else:
                 print('Invalid usage for push command')
                 print(USAGE)
                 exit(1)
         elif arg == 'pull':
             if len(args) == 0:
-                op = OpType.PULL
+                action = OpType.PULL
             elif len(args) > 0:
-                op = OpType.PULL
-                path = args.pop()
+                action = OpType.PULL
+                file_path = args.pop()
             else:
                 print('Invalid usage for pull command')
                 print(USAGE)
@@ -129,21 +133,13 @@ def load_config_from_args(args: List[str]) -> Tuple[TodistAccount, str, OpType, 
                 print('Invalid usage for `-p` subcommand')
                 print(USAGE)
                 exit(1)
-        elif arg == '-config':
-            if len(args) > 0:
-                config_path = args.pop()
-                return load_config_from_json(config_path)
-            else:
-                print('Invalid usage for `-config` subcommand')
-                print(USAGE)
-                exit(1)
         else:
             print(f'Unknwon command {arg}')
             exit(1)
     else:
         print(f'[INFO] Loading account from file: `{account_path}`')
-        account = load_account_from_json(account_path)
-    return account, path, op, project_name
+        acc = load_account_from_json(account_path)
+    return acc, file_path, action, project_name
 
 
 USAGE = """USAGE:
@@ -158,16 +154,16 @@ USAGE = """USAGE:
     -p <project_name>: Choose todist project to work from"""
 
 if __name__ == '__main__':
-    args = list(reversed(sys.argv[1:].copy()))
+    arguments = list(reversed(sys.argv[1:].copy()))
     op: Optional[OpType] = None
     custom: bool = False
     config_path = 'config.tobedone.json'
     config = False
-    while len(args) > 0:
-        arg = args.pop()
-        if arg == '--config':
-            if len(args) > 0:
-                config_path = args.pop()
+    while len(arguments) > 0:
+        argument = arguments.pop()
+        if argument == '--config':
+            if len(arguments) > 0:
+                config_path = arguments.pop()
             if os.path.exists(config_path):
                 config = True
             else:
@@ -176,16 +172,16 @@ if __name__ == '__main__':
                 exit(1)
     if config:
         print(f'[INFO] Loading config from file: `{config_path}`')
-        account, path, op, project_name = load_config_from_json(config_path)
+        account, path, op, p_name = load_config_from_json(config_path)
     else:
-        account, path, op, project_name = load_config_from_args(args)
+        account, path, op, p_name = load_config_from_args(arguments)
     succeeded: bool = False
     if op == OpType.PUSH:
-        print(f'[INFO] Pushing file `{path}` to todoist. Project: {project_name}')
-        succeeded = update_todist_from_file(account, path, project_name)
+        print(f'[INFO] Pushing file `{path}` to todoist. Project: {p_name}')
+        succeeded = update_todist_from_file(account, path, p_name)
     elif op == OpType.PULL:
-        print(f'[INFO] Pulling to file `{path}` from todoist. Project: {project_name}')
-        succeeded = update_file_from_todoist(account, path, project_name)
+        print(f'[INFO] Pulling to file `{path}` from todoist. Project: {p_name}')
+        succeeded = update_file_from_todoist(account, path, p_name)
     else:
         raise ValueError(f'Unknown operation {op}. This may be a bug in the parsing of the arguments')
     if succeeded:
